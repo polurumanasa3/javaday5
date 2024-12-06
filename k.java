60dc45b47d7e399678b257ac316fc57e55f8752e
polurumanasa3 <manasap142@gmail.com>
2024-12-06 00:48:05 -0800

    Add files via upload

---
+++ k.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class k   
+{
+public static void main(String args[])
+{
+Scanner Sc=new Scanner(System.in);
+int n=Sc.nextInt();
+ for(int i=n;i>=1;i--)
+ {
+    for(int j=n;j>=1;j--)
+    {
+        System.out.print(j+" ");
+    }
+    System.out.println();
+ }
+}
+}
+   
